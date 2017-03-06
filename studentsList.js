1eea3656bfd98c5a98d9fd99749eb3fcb53a739b
Philippe Pinceloup <ph.pinceloup@gmail.com>
2017-03-06 11:48:47 +0100

    import students.js to  studentsList

--- studentsList.js
+++ studentsList.js
@@ -1,1 +1,15 @@
 // Write yout Javascript code in this files
+var request = new XMLHttpRequest(); // on crée une instance de la fonction HTTPRequest. Je la stocke dans la variable request
+request.open('GET', 'students.json', true); // On déclare où on fait la rzquête. GET = je veux obtenir des données. TRUE = les requêtes peuvent se faire après le chargement de la page
+request.onreadystatechange = function (aEvt) { // cette fonction s'exécute à chaque fois que readyState change
+  if (request.readyState == 4) { // 4 = requête terminée
+     if(request.status == 200) { // tout s'est bien passé
+      var students = JSON.parse(request.responseText);
+      console.log(students);
+    } else {
+      dump("Erreur pendant le chargement de la page.\n");
+    } // sinon affiche cette erreur
+  }
+};
+
+request.send();
